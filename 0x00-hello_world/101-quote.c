c8433f384082563594af90635ec6c10981e5d40c
ds-aurelle1 <aurelle.adjahou@gmail.com>
2022-12-08 19:26:21 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,13 @@
-#include<unistd.h>
-
-
-/*
+#include <unistd.h>
+/**
  * main - Entry point
  *
- * Description: print a quote using write function
- * ssize_t write(int fd, const void *buf, size_t count);
- *
- * Return: This time we return an error 1.
- *
-*/
-
-
-
+ * Return: Always 1 (Success)
+ */
 int main(void)
-
 {
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
-
+	write(2, str1, 59);
 	return (1);
-
 }
