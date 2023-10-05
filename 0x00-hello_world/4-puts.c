25058367a2e04305702891d9ed4c20d84fbcf0d3
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-05 10:12:23 -0700

    my first C program with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /*
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * prints exactly with proper grammar, but the outcome is a piece of art,
  * main void() -- entry point
  * only use puts function
  * return 0;
  */
 
 int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return(0);
 
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
