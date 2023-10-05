eaf1c0fe983f01118c78bb24e87fde090ff43e89
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-05 09:57:29 -0700

    first C program commited using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * main void() -- entry point
+ * only use puts function
+ * return 0;
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return(0);
+
+}
