b63910a0173aaafb33dac1f9e474fac44f55a31f
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-17 00:58:47 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Describe: write _putchar
+ * Return: Always 0
+ */
+ 
+int main(void)
+{
+char a[8] ="_putchar";
+int b=0; 
+ while (b<8)
+   {
+     putchar(a[b]);
+     b++;
+   }
+    
+   putchar('\n');
+ 
+return (0);
+}
