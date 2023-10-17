f9705822d7ce92a5a6e86474036ab4a351e4a4ef
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-17 01:06:12 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,18 +6,18 @@
  * Describe: write _putchar
  * Return: Always 0
  */
- 
+
 int main(void)
 {
-char a[8] ="_putchar";
-int b=0; 
-while (b<8)
+char a[8] = "_putchar";
+int b = 0;
+while (b < 8)
 {
 putchar(a[b]);
 b++;
 }
-    
+
 putchar('\n');
- 
+
 return (0);
 }
