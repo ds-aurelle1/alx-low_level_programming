955f0c1f357e000a5524aae1d946c8124a3f37b3
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-17 12:53:01 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers -print all numbers from 0 to 9
+*Return: void
+*/
+void print_numbers(void)
+{
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
+}
