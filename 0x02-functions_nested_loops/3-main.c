15688fb4a7244fb8ebc4e03f546737dfd9dd7daa
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-17 11:39:53 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-    int r;
+int r;
 
-    r = _islower('H');
-    putchar(r + '0');
-    r = _islower('o');
-    putchar(r + '0');
-    r = _islower(108);
-    putchar(r + '0');
-    putchar('\n');
-    return (0);
+r = _islower('H');
+putchar(r + '0');
+r = _islower('o');
+putchar(r + '0');
+r = _islower(108);
+putchar(r + '0');
+putchar('\n');
+return (0);
 }
