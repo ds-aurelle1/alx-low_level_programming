351be3e61fe3322331ba9ff5fe26596018c9e2c3
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-17 10:45:24 -0700

    checker if a char islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_islower - check if a char is lower
+ *c: should be verified
+ *return: 0 or 1
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+
+return (1);
+
+return (0);
+}
