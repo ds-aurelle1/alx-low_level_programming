38ff3fa45495833f10ae34354d2f07882715e0a4
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-17 01:00:51 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,13 +11,13 @@ int main(void)
 {
 char a[8] ="_putchar";
 int b=0; 
- while (b<8)
-   {
-     putchar(a[b]);
-     b++;
-   }
+while (b<8)
+{
+putchar(a[b]);
+b++;
+}
     
-   putchar('\n');
+putchar('\n');
  
 return (0);
 }
