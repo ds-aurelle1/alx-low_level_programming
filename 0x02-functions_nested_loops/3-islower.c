d689c1ca91aaf6dc2241ec227d3aee9f3c581aed
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-20 05:17:54 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase character
+ * @c: parameter that return int value
+ * Return: 1 if it's lowercase or 0 else
+ */
+
+int _islower(int c)
+{
+if islower(c)
+{
+return (1);
+
+return (0);
+}
+}
