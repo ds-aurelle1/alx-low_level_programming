d091b8e5f42939b7cb7536b4aa4009a2f2fd75fb
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-20 05:09:39 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,13 @@
 /**
  * print_alphabet - print alphabet in lowercase
  * _putchar: should be use 2 times max
- * Return: 0
+ * Return: void
  */
 
 void print_alphabet(void)
 {
 char i;
-for (i = 'a'; i < 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
