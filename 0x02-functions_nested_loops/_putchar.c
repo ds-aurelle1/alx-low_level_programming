e2ee7cbae81731bb3d333ac2be8db35c51457705
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-20 00:01:59 -0700

    3-islower.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,6 +2,7 @@
 
 /**
  * _putchar - writes the character c to stdout
+ * @c - parameter the function takes
  * Return: On success 1.
  * On error, -1 is returned, and errno is set approprialtely.
  */
