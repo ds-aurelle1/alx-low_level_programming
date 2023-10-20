aff4cfe355d80a963f804f7ba1e67bbf2d176ff9
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-20 19:22:19 +0100

    sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,16 @@ _putchar('+');
 return (1);
 }
 
-else if (n < 0)
+else if (n == 0)
 {
-_putchar('-');
-return (1);
+_putchar('0');
+return (0);
 }
 
 else
 {
-return (0);
+_putchar('-');
+return (1);
 }
 _putchar('\n');
 return (0);
