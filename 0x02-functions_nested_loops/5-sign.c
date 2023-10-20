4f442d89fb60da12aca6ffe52aa282320b5a5bbf
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-20 19:14:07 +0100

    sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,18 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+%d",1);
+_putchar('+');
+return 1;
 }
 
 else if (n < 0)
 {
-printf("-%d",1);
+_putchar('-');
+return 1;
 }
 
 else
 {
-printf("%d",0);
+return 0;
 }
-
-return (0);
 }
