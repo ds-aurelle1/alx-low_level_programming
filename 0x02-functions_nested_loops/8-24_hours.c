598b920d16d909609f6d8968237e1e6725025830
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-23 09:45:56 +0100

    printed hour and minute of jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ *
+ * Return: void so nothing
+ */
+
+void jack_bauer(void)
+{
+int hour = 0;
+int minute = 0;
+
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+printf("%02d:%02d\n", hour, minute);
+}
+}
+
+}
