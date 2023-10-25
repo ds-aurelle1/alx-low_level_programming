2ac45c54b5282d0eee5469b33bc934760b080004
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-25 19:26:50 +0100

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,29 +6,29 @@
 *Return:returns the capitalized string
 */
 
-char *cap_string(char *stri)
+char *cap_string(char *str)
 {
 int index = 0;
 
-while (stri[++index])
+while (str[++index])
 {
-while (!(stri[index] >= 'a' && stri[index] <= 'z'))
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 
-if (stri[index - 1] == ' ' ||
-stri[index - 1] == '\t' ||
-stri[index - 1] == '\n' ||
-stri[index - 1] == ',' ||
-stri[index - 1] == ';' ||
-stri[index - 1] == '.' ||
-stri[index - 1] == '!' ||
-stri[index - 1] == '?' ||
-stri[index - 1] == '"' ||
-stri[index - 1] == '(' ||
-stri[index - 1] == ')' ||
-stri[index - 1] == '{' ||
-stri[index - 1] == '}')
-stri[index] -= 32;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}')
+str[index] -= 32;
 }
-return (stri);
+return (str);
 }
