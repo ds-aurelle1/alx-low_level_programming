be85ba7a925bfdbb591877edbc8dd17e9f0c1f2b
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-26 09:30:50 +0100

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,34 +7,33 @@
 *Return:returns the capitalized string
 */
 
-char* cap_string(char* str) 
+char *cap_string(char *str)
 {
-	int i = 0;
-	int capitalize = 1; /* Flag to indicate if the next character should be capitalized */
+int i = 0;
+int capitalize = 1; /*say if next character should be capitalized*/
 
-    for (i = 0; str[i] != '\0'; i++) 
-    {
-        if(str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
-           str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
-           str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-           str[i] == '}') 
-	{
-            capitalize = 1;
-        } 
-	else 
-	{
-            if (capitalize) 
-	    {
-                str[i] = toupper(str[i]);
-                capitalize = 0;
-            } 
-	    else 
-	    {
-                str[i] = tolower(str[i]);
-            }
-        }
-    }
-
-    return (str);
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
+str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
+str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+str[i] == '}')
+{
+capitalize = 1;
+}
+else
+{
+if (capitalize)
+{
+str[i] = toupper(str[i]);
+capitalize = 0;
+}
+else
+{
+str[i] = tolower(str[i]);
+}
+}
+}
+return (str);
 }
 
