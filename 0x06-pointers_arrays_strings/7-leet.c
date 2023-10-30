f11be2851ddd73caaa6e11b02e778569f93d48b5
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-10-30 20:26:55 +0100

    encode into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,17 +7,41 @@
 
 char *leet(char *str)
 {
-int index1 = 0, index2;
-char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-while (str[++index1])
+char *leet_chars = "aAeEoOtTlL";
+char *leet_equiv = "4433007711";
+
+while (*str != '\0')
 {
-for (index2 = 0; index2 <= 7; index2++)
+int i = 0;
+int found = 0;
+
+while (leet_chars[i] != '\0')
 {
-if (str[index1] == leet[index2] ||
-str[index1] - 32 == leet[index2])
-str[index1] = index2 + '0';
+if (*str == leet_chars[i])
+{
+*str = leet_equiv[i];
+found = 1;
+break;
+}
+i++;
+}
+if (!found)
+{
+i = 0;
+while (leet_chars[i] != '\0')
+{
+if (*str == leet_chars[i] + 32)
+{
+*str = leet_equiv[i];
+break;
+}
+i++;
 }
 }
+
+str++;
+}
+
 return (str);
 }
