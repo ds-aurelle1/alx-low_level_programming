46cc119cb26c2801fda7f9af9f80a8ef34ede2ea
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-11-01 14:26:50 +0100

    encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,45 +1,22 @@
-
 #include "main.h"
-
-/**
-* rot13 -  encodes a string using rot13
-* @str:the string targeted
-*Return: returns the encoded string
-*/
+#include <stdio.h>
 
 char *rot13(char *str)
 {
-int index1, index2;
+int i =0;
 
-char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-									 'G', 'H', 'I', 'J', 'K', 'L',
-									 'M', 'N', 'O', 'P', 'Q', 'R',
-									 'S', 'T', 'U', 'V', 'W', 'X',
-									 'Y', 'Z', 'a', 'b', 'c', 'd',
-									 'e', 'f', 'g', 'h', 'i', 'j',
-									 'k', 'l', 'm', 'n', 'o', 'p',
-									 'q', 'r', 's', 't', 'u', 'v',
-									 'w', 'x', 'y', 'z'};
+while (str[i])
+{
+if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+{
+str[i] += 13;
+}
+else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
+{
+str[i] -= 13;
+}
+i++;
+}
 
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-									 'T', 'U', 'V', 'W', 'X', 'Y',
-									 'Z', 'A', 'B', 'C', 'D', 'E',
-									 'F', 'G', 'H', 'I', 'J', 'K',
-									 'L', 'M', 'n', 'o', 'p', 'q',
-									 'r', 's', 't', 'u', 'v', 'w',
-									 'x', 'y', 'z', 'a', 'b', 'c',
-									 'd', 'e', 'f', 'g', 'h', 'i',
-									 'j', 'k', 'l', 'm'};
-while (str[++index1])
-{
-for (index2 = 0; index2 < 52; index2++)
-{
-if (str[index1] == alphabet[index2])
-{
-str[index1] = rot13key[index2];
-break;
-}
-}
-}
 return (str);
 }
