f7e76cff75e73f9f6cb2f60f5bfadbfd0809a15e
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-11-01 12:32:57 +0100

    encode in 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,32 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* leet - function that encode a string
-*@str:string that will be encoded
-*Return:returns encoded string
-*/
+ * leet - function that encode a string
+ * @str:string that will be encoded
+ * Return: encoded string
+ */
 
 char *leet(char *str)
 {
-char leet_equiv[] = "4433117700";
+char leet_equiv[] = "4433007711";
+int j = 0;
 
-while (*str)
+while (str[j])
 {
 int i = 0;
-while (leet_equiv[i]) 
+while (leet_equiv[i])
 {
-if (*str == "aAeElLtToO"[i])
+if (str[j] == "aAeEoOtTlL"[i])
 {
-*str = leet_equiv[i];
+str[j] = leet_equiv[i];
 break;
 }
 i++;
 }
-str++;
+j++;
 }
+
 return (str);
 }
+
