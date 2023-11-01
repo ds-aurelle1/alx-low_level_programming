16332f0b2b2ed39eda51989b0744e97004f493bf
ds-aurelle1 <aurelle.adjahou@gmail.com>
2023-11-01 12:06:50 +0100

    encode in 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,41 +7,21 @@
 
 char *leet(char *str)
 {
+char leet_equiv[] = "4433117700";
 
-char *leet_chars = "aAeEoOtTlL";
-char *leet_equiv = "4433007711";
-
-while (*str != '\0')
+while (*str)
 {
 int i = 0;
-int found = 0;
-
-while (leet_chars[i] != '\0')
+while (leet_equiv[i]) 
 {
-if (*str == leet_chars[i])
-{
-*str = leet_equiv[i];
-found = 1;
-break;
-}
-i++;
-}
-if (!found)
-{
-i = 0;
-while (leet_chars[i] != '\0')
-{
-if (*str == leet_chars[i] + 32)
+if (*str == "aAeElLtToO"[i])
 {
 *str = leet_equiv[i];
 break;
 }
 i++;
 }
-}
-
 str++;
 }
-
 return (str);
 }
